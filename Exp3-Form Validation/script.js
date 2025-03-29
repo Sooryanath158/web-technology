37560a1a61d3e22541e15f7eb1a74f62ab98f20e
Sooryanath S D <231501158@rajalakshmi.edu.in>
2025-03-29 14:06:40 +0530

    script.js

--- Exp3-Form Validation/script.js
+++ Exp3-Form Validation/script.js
@@ -1,29 +1,28 @@
 function validate(){
-    //first name
+  
     var fn = document.getElementById("first_name").value;
     if (!fnval(fn)){
         alert("Enter the first name atlest longer than 3 character and only contain letters");
         return false;
     }
-    //last name 
+  
     var ln = document.getElementById("last_name").value;
     if(ln.length == 0){
         alert("Please Enter the last name");
         return false;
     }
-    //phone number
+ 
     var ph = document.getElementById("phnumber").value;
     if(typeof(ph) != 'number' && ph.length != 10){
         alert("Please Enter the correct phone number with 10 digits");
         return false;
     }
-    //Password 
+   
     var passw = document.getElementById("pass").value;
     if(!passval(passw)){
         alert("Enter a password with a uppercase,number,special character and it need to be atlease 8 character in it");
         return false;
     }
-    //address
     var add = document.getElementById("address").value;
     if(add.length == 0){
         alert("Please Enter your address");
